1d4d7781ccf46afdee30b647a948661b3120fdb8
jaydeep <ptljaydeep4@gmail.com>
2022-02-12 18:17:33 +0530

    - Latest enhansment changes

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  submitted: boolean = false;
+  emailForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.emailForm = this.formBuilder.group({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[A-Za-z ]+'),
+      ]),
+      email: new FormControl(
+        '',
+        Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+      ),
+      nftForHome: new FormControl(),
+      homeAsNft: new FormControl(),
+    });
   }
 
+  saveForm() {
+    this.submitted = true;
+    if (this.emailForm.valid) {
+      window.scroll(0, 0);
+      this.submitted = false;
+      this.emailForm.reset();
+    }
+  }
 }
