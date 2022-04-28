fc1144ce09fb568904421608f20c7df8f2cb3da9
Jaydeep <ptljaydeep4@gmail.com>
2022-04-28 18:52:24 +0530

    - created details service and fire API call for fetch details data

---
+++ src/app/views/marketplace/details/details.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DetailsService {
+
+  constructor(private http: HttpClient) { }
+
+  nftData(id: any) {
+    return this.http.get(environment.apiUrl + `/nft/${id}` );
+  }
+}
