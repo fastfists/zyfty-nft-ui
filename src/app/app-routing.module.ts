21843414cd83f166f0204c4fa9a3ec0867d1d0c1
Jaydeep <ptljaydeep4@gmail.com>
2022-06-13 11:23:37 +0530

    NFTs are happening NOW routing issue fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,8 +19,10 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
+      useHash: true,
       scrollPositionRestoration: 'enabled',
       anchorScrolling: 'enabled',
+      onSameUrlNavigation: "reload",
     }),
   ],
   exports: [RouterModule],
