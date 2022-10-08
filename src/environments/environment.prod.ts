e996b3025919e6443c851bf9dc8244e42435acaa
jaydeep4 <ptljaydeep4@gmail.com>
2022-10-08 11:30:21 +0530

    - Added map key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.dev.zyfty.io/'
+  apiUrl: 'https://api.dev.zyfty.io/',
+  map_api_key: 'AIzaSyAfXLngkdMTHRaUbbPw8ya8m_I7AzLAjMo&address'
 };
