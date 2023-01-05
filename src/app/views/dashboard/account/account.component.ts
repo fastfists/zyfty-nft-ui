2ae0c0e6a932816154ad2d3f4fd7dbf679e21e99
Denzell Ford <fdenzell@gmail.com>
2023-01-05 14:10:20 -0600

    Updated account page with new design

--- src/app/views/dashboard/account/account.component.ts
+++ src/app/views/dashboard/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, Injectable, OnInit } from '@angular/core';
+import { info } from 'console';
 import { TokenService } from 'src/app/common-service/contracts/token.service';
 import { WalletProvider } from 'src/app/common-service/provider/provider.service';
 
@@ -13,12 +14,30 @@ export class AccountComponent implements OnInit {
 
   account = this.provider.account;
   balance = 0;
-  constructor(private provider: WalletProvider, private token: TokenService) { }
+  symbol = "";
+  name = "";
+  infoLoaded = false;
+
+  constructor(private provider: WalletProvider, private token: TokenService) {
+  }
 
   ngOnInit(): void {
-    this.token.balance().then((balance) => {
-      this.balance = balance;
-    })
+    this.account.subscribe({
+      next: async (account) => {
+        if (account != null) {
+          [this.balance, this.symbol, this.name] = await this.loadTokenInfo();
+          this.infoLoaded = true;
+        }
+      }
+    });
+  }
+
+  async loadTokenInfo(): Promise<[number, string, string]> {
+    return [
+      await this.token.balance(),
+      await this.token.symbol(),
+      await this.token.name()
+    ]
   }
 
 }
